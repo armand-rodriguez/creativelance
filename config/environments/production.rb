cb85a42919b3d098a1ff07029595a515d88b08e2
Armand <armand.j.rodriguez@gmail.com>
2019-04-09 12:13:42 -0700

    configure devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.active_storage.service = :amazon
+  config.action_mailer.default_url_options = { host: 'https://connectly.herokuapp.com' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations', confirmations: 'confirmations' }
   resources :accounts
   resources :freelancers
   resources :panels
