eff516ce4405f14b67c9f9e2c18e51af94a5284b
Armand <armand.j.rodriguez@gmail.com>
2019-04-09 12:31:40 -0700

    configure confirmations controller v2

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     super
-    
+    # if @user.save
+    #   Panel.create!(user_id: current_user.id)
+    #   PublicProfile.create!(user_id: current_user.id)
+    # end
   end
 
   protected
