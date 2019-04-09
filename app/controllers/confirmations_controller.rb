b0d5ddfc3022fa7001834dc114dab5d0e0ad8b21
Armand <armand.j.rodriguez@gmail.com>
2019-04-09 12:27:58 -0700

    configure confirmations controller

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,6 +3,8 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
+    Panel.create!(user_id: current_user.id)
+    PublicProfile.create!(user_id: current_user.id)
     root_path
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     super
-    if @user.save
-      Panel.create!(user_id: current_user.id)
-      PublicProfile.create!(user_id: current_user.id)
-    end
+    
   end
 
   protected
